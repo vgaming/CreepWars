3db3a448e80773268f40fc1979d327799fff0c85
Vasya N <n1m5spm@yandex.com>
2017-11-19 14:39:07 +0300

    increase creep score scaling

--- lua/config.lua
+++ lua/config.lua
@@ -4,7 +4,7 @@ creepwars_lvl0_barrier = 12 -- creep score lower than this value will generate l
 creepwars_lvl3plus_barrier = 50
 
 creepwars_score_power = 0.6
-creepwars_score_scale = 0.057
+creepwars_score_scale = 0.065
 creepwars_score_start = 9
 
 creepwars_score_for_kill = function(unit) return math.pow(unit.__cfg.cost, 0.6) * creepwars_score_scale end

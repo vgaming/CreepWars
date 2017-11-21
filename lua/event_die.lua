b851e46c405111d91ef8603deb28973c28b9c019
Vasya N <n1m5spm@yandex.com>
2017-11-21 14:41:49 +0300

    fix unit duying handling

--- lua/event_die.lua
+++ lua/event_die.lua
@@ -5,10 +5,20 @@ local creepwars_creep_kill_event = creepwars_creep_kill_event
 
 local defender = wesnoth.get_unit(wesnoth.get_variable("x1"), wesnoth.get_variable("y1"))
 local attacker = wesnoth.get_unit(wesnoth.get_variable("x2"), wesnoth.get_variable("y2"))
-if attacker ~= nil and (defender.canrecruit or defender.variables["creepwars_creep"]) then
+if attacker == nil then
+	local msg = "Warning: Unit died without attacker. This is unexpected. " ..
+		"No creep score or gold bonus will be generated. " ..
+		"This is probably because of a conflicting addon/modification. " ..
+		"If the host has no modifications, please report the issue."
+	print(msg)
+	wesnoth.message("Creep Wars", msg)
+elseif defender.canrecruit or defender.variables["creepwars_creep"] then
 	creepwars_creep_kill_event(attacker, defender)
 else
-	error("Unit died without attacker! This is bad! Maybe it's the latest guard kill that is untested?")
+	local turn_number = wesnoth.get_variable("turn_number")
+	local msg = "Info: " .. defender.type .. " died and it is neither Leader nor Creep. Probably a plagued unit. No gold/creep bonus will be generated. Turn " .. turn_number
+	print(msg)
+	wesnoth.message("Creep Wars", msg)
 end
 
 -- >>

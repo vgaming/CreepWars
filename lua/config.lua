858c783d93ff20875509d3aff7cc639576f0541b
Vasya N <n1m5spm@yandex.com>
2017-12-19 22:08:22 +0300

    fix leader hiding

--- lua/config.lua
+++ lua/config.lua
@@ -24,7 +24,7 @@ if mirror_style == "mirror" then
 elseif wesnoth and wesnoth.get_variable("creepwars_hide_leaders") ~= nil then
 	hide_leaders = wesnoth.get_variable("creepwars_hide_leaders")
 else
-	array_forall(wesnoth and wesnoth.sides or {}, function(side)
+	hide_leaders = not array_forall(wesnoth and wesnoth.sides or {}, function(side)
 		return side.controller == "human" or side.controller == "ai" or side.controller == "null"
 	end)
 end

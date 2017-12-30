3fbb50c172681b0e6e865b4d0bc6d2e5303809a7
Vasya N <n1m5spm@yandex.com>
2017-12-30 12:43:32 +0300

    unit_analyze: common code

---
+++ lua/unit_analyze/unit_analyze_common.lua
@@ -0,0 +1,62 @@
+-- << unit_analyze_common
+
+local wesnoth = wesnoth
+local creepwars = creepwars
+local ipairs = ipairs
+local helper = wesnoth.require("lua/helper.lua")
+
+
+-- all Default Era units except Ulf
+local default_era_leaders = {
+	"Vampire Bat", -- costy lvl0
+	"Drake Burner", "Drake Clasher", "Drake Fighter", "Drake Glider", "Saurian Augur", "Saurian Skirmisher", -- lvl1 drakes
+	"Dwarvish Fighter", "Dwarvish Guardsman", "Dwarvish Thunderer", "Gryphon Rider", "Footpad", "Poacher", "Thief", -- lvl1 knalgan
+	"Bowman", "Cavalryman", "Fencer", "Heavy Infantryman", "Horseman", "Mage", "Merman Fighter", "Spearman", -- lvl1 loyal
+	"Naga Fighter", "Orcish Archer", "Orcish Assassin", "Orcish Grunt", "Troll Whelp", "Wolf Rider", -- lvl1 orc
+	"Elvish Archer", "Elvish Fighter", "Elvish Scout", "Elvish Shaman", "Mage", "Merman Hunter", "Wose", -- lvl1 rebels
+	"Dark Adept", "Ghost", "Ghoul", "Skeleton Archer", "Skeleton" -- lvl1 undead
+}
+
+local default_era_creep_base = creepwars.array_merge(default_era_leaders,
+	{ "Peasant", "Woodsman", "Ruffian", "Goblin Spearman", "Dwarvish Ulfserker" })
+
+
+local function add_advances(arr, set, filter)
+	set = set or creepwars.array_to_set(arr)
+	filter = filter or function(adv) return true end
+	for _, unit in ipairs(arr) do
+		for _, adv in ipairs(creepwars.split_comma(wesnoth.unit_types[unit].__cfg.advances_to)) do
+			if set[adv] == nil and wesnoth.unit_types[adv] and filter(adv) then
+				set[adv] = true
+				arr[#arr + 1] = adv
+			end
+		end
+	end
+end
+
+
+local function unit_count_specials(unit)
+	local result = {}
+	for attack in helper.child_range(wesnoth.unit_types[unit].__cfg, "attack") do
+		for specials in helper.child_range(attack, "specials") do
+			for _, special in ipairs(specials) do
+				local name = special[1]
+				if name == "chance_to_hit" then
+					result[name] = special[2]["value"]
+				else
+					result[name] = (result[name] or 0) + 1
+				end
+			end
+		end
+	end
+	return result
+end
+
+
+creepwars.unit_count_specials = unit_count_specials
+creepwars.add_advances = add_advances
+creepwars.default_era_leaders = default_era_leaders
+creepwars.default_era_creep_base = default_era_creep_base
+
+
+-- >>
---
+++ lua/unit_analyze/unit_analyze_wesnoth12.lua
@@ -0,0 +1,47 @@
+-- << unit_analyze_wesnoth12
+
+-- This file provides function to generate Creeps with expected cost.
+-- See end of file for the function itself, `creepwars_generate_creep`
+
+local wesnoth = wesnoth
+local creepwars = creepwars
+local creepwars_creep_lvl_max = creepwars_creep_lvl_max
+
+
+local leader_array = creepwars.default_era_leaders
+
+
+local creep_array = creepwars.default_era_creep_base
+
+creepwars.add_advances(creep_array, nil, function(adv)
+	return wesnoth.unit_types[adv].level <= creepwars_creep_lvl_max
+end)
+
+
+local leader_strength = {}
+for _, unit in ipairs(leader_array) do
+	local type = wesnoth.unit_types[unit]
+	leader_strength[unit] = type.cost * (14 + type.max_hitpoints / 2) / type.max_hitpoints
+end
+
+
+-- lvl1, no "plague", cannot advance to "berserk"
+local function can_be_a_leader(base_unit)
+	local arr = { base_unit }
+	creepwars.add_advances(arr)
+	for _, adv in ipairs(arr) do
+		if creepwars.unit_count_specials(adv)["berserk"] ~= nil then
+			return false
+		end
+	end
+	return wesnoth.unit_types[base_unit].level == 1 and count_specials(base_unit)["plague"] == nil
+end
+
+
+creepwars.can_be_a_leader = can_be_a_leader
+creepwars.creep_array = creep_array
+creepwars.leader_strength = leader_strength
+creepwars.recruitable_array = leader_array
+
+
+-- >>

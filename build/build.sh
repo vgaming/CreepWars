32ea950745ee1e8e0a7f0f4819dbed8ee93580c8
Vasya N <n1m5spm@yandex.com>
2018-01-12 01:07:58 +0300

    shorten version text

--- build/build.sh
+++ build/build.sh
@@ -5,7 +5,7 @@ set -o pipefail
 mkdir -p target
 
 
-git describe --tags | tee target/version.txt
+git describe --tags | head -c 11 | tee target/version.txt
 
 
 lua build/docs_to_txt.lua > target/about.txt

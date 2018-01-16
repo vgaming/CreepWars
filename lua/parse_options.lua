e26f52bace51816b15257739e62eada747e5eae9
Vasya N <n1m5spm@yandex.com>
2018-01-17 01:01:17 +0300

    fix [options] saving on wesnoth-1.13

--- lua/parse_options.lua
+++ lua/parse_options.lua
@@ -6,8 +6,9 @@ local creepwars = creepwars
 
 -- ugly hack to support wesnoth-1.12
 -- (it cannot work with multiple scenarios defining same option id-s)
-local scenario_suffix = wesnoth.game_config.mp_settings.mp_scenario
-
+local scenario_suffix = wesnoth.compare_versions(wesnoth.game_config.version, ">=", "1.13.0")
+	and ""
+	or wesnoth.game_config.mp_settings.mp_scenario
 
 local mirror_style = wesnoth.get_variable("creepwars_mirror_style" .. scenario_suffix) or "manual"
 

ae4b7ce74b09fefff6a02fcdf9ff760a17cf579d
Vasya N <n1m5spm@yandex.com>
2018-01-17 01:17:03 +0300

    escape scenario ID to avoid save/load problems

--- build/build.sh
+++ build/build.sh
@@ -7,6 +7,7 @@ mkdir -p target
 
 # remove newline, cut out commit hash
 git describe --tags | cut -d '-' -f -2 | tr -d '\n' | tee target/version.txt
+cat target/version.txt | tr '.-' '__' > target/version_nodot.txt
 
 
 lua build/docs_to_txt.lua > target/about.txt

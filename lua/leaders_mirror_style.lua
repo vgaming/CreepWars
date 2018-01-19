75338907c0875f97a5713ebf0c44c0860aa2334e
Vasya N <n1m5spm@yandex.com>
2018-01-19 17:46:51 +0300

    add mirror overwrite warning

--- lua/leaders_mirror_style.lua
+++ lua/leaders_mirror_style.lua
@@ -5,6 +5,7 @@ local creepwars = creepwars
 local ipairs = ipairs
 local math = math
 local helper = wesnoth.require("lua/helper.lua")
+local T = wesnoth.require("lua/helper.lua").set_wml_tag_metatable {}
 local array_map = creepwars.array_map
 local format = creepwars.format
 local is_ai_array = creepwars.is_ai_array
@@ -12,11 +13,25 @@ local mirror_style = creepwars.mirror_style
 local recruitable_array = creepwars.recruitable_array
 
 
-local function set_type(unit, type)
-	wesnoth.transform_unit(unit, type)
-	unit.attacks_left = unit.max_attacks
-	unit.hitpoints = unit.max_hitpoints
-	unit.moves = unit.max_moves
+local function leaders_mirror_show_warning()
+	creepwars.wesnoth_message("Game rules have <b>" .. mirror_style .. " leader</b> option.\n\n"
+		.. "Your leader (side " .. wesnoth.current.side .. ") was replaced to allow fair game play.")
+end
+
+
+local function set_type(unit, type, is_downgrade)
+	if unit.type ~= type then
+		wesnoth.transform_unit(unit, type)
+		unit.attacks_left = unit.max_attacks
+		unit.hitpoints = unit.max_hitpoints
+		unit.moves = unit.max_moves
+		if is_downgrade == false and wesnoth.sides[unit.side].__cfg.chose_random == false then
+			wesnoth.wml_actions.event {
+				name = ("side %s turn 1"):format(unit.side),
+				T.lua { code = "creepwars.leaders_mirror_show_warning()" }
+			}
+		end
+	end
 end
 
 
@@ -29,12 +44,13 @@ local function downgrade_leaders()
 		local downgrade_array = creepwars.unit_downgrades(unit.type)
 		if is_ai_array[unit.side] ~= true
 			and downgrade_array and #downgrade_array > 0
-			and wesnoth.unit_types[unit.type].level == 2 then
+			and wesnoth.unit_types[unit.type].level == 2
+		then
 			local downgrade = downgrade_array[1] -- need the same for true mirror
 			if creepwars.can_be_a_leader(downgrade) then
-				set_type(unit, downgrade)
+				set_type(unit, downgrade, true)
 			else
-				set_type(unit, random_leader())
+				set_type(unit, random_leader(), false)
 			end
 		end
 	end
@@ -50,7 +66,7 @@ local function set_all_leaders(unit_array_function)
 		for side_in_team_index, side_number in ipairs(side_array) do
 			local unit_type = unit_array[math.fmod(side_in_team_index - team_index + #side_array, #side_array) + 1]
 			for _, unit in ipairs(wesnoth.get_units { canrecruit = true, side = side_number }) do
-				set_type(unit, unit_type)
+				set_type(unit, unit_type, false)
 			end
 		end
 	end
@@ -111,6 +127,9 @@ elseif mirror_style == "same_cost" then
 else
 	error("Unknown mirror style: " .. format(mirror_style))
 end
+
+
+creepwars.leaders_mirror_show_warning = leaders_mirror_show_warning
 
 
 -- >>

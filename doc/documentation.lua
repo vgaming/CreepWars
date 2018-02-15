1484cfa1fd2ed5cbc8596d2b892972caacda3933
Vasya N <n1m5spm@yandex.com>
2018-02-15 23:45:12 +0300

    fix game reload on turn 1
    first_turn_advantage being at fault previously

--- doc/documentation.lua
+++ doc/documentation.lua
@@ -40,7 +40,7 @@ if wesnoth then
 		text = "<span color='#FFFFFF'>Mirror style: " .. creepwars.mirror_style .. "</span>"
 	}
 
-	local recent = 'Recent changes: removed "berserker" creeps, gold multiplier for leaderkill increased 4->5.'
+	local recent = 'Recent changes: removed "berserker" creeps.'
 	wesnoth.message("Creep Wars", "Press Ctrl J to see game rules. " .. recent)
 
 	local T = wesnoth.require("lua/helper.lua").set_wml_tag_metatable {}

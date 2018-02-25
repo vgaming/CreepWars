c826ea895c4cf72b5ca2f8d8d0b942e1605348f3
Vasya N <n1m5spm@yandex.com>
2018-02-25 09:51:25 +0300

    rename file

--- .build/config.sh
+++ .build/config.sh
@@ -1,6 +1,6 @@
 #### If you want a custom description to be put in _server.pbl, override this method
 description() {
-	lua .build/docs_to_txt.lua
+	lua .build/generate_server_description.lua
 }
 
 upload_to_wesnoth_versions=(1.12 1.13)

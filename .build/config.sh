57436f8bbe44d6c0e56976bba31d42d643d67e61
Vasya N <n1m5spm@yandex.com>
2018-03-21 11:55:18 +0300

    restore target/about.txt generation

--- .build/config.sh
+++ .build/config.sh
@@ -3,7 +3,7 @@ not_pushed_ignore=true
 #### If you want a custom description to be put in _server.pbl, override this method
 description() {
 	cd Creep_War_Dev
-	lua ../.build/generate_server_description.lua
+	lua ../.build/generate_server_description.lua | tee target/about.txt
 	cd ..
 }
 

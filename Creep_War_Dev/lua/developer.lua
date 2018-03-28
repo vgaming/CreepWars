aa4653c435e732669fc5afbf1472fc115a09fc50
Vasya N <n1m5spm@yandex.com>
2018-03-28 09:51:45 +0300

    simplify CLI parsing

--- Creep_War_Dev/lua/developer.lua
+++ Creep_War_Dev/lua/developer.lua
@@ -1,15 +1,13 @@
 -- << developer
 
 local wesnoth = wesnoth
-local creepwars = creepwars
-local ipairs = ipairs
 local string = string
 local tostring = tostring
 
 --- Allows configuring game from command-line
 
 local cli_string = tostring(wesnoth.sides[1].user_team_name)
-for _, assignment in ipairs(creepwars.split_comma(cli_string)) do
+for assignment in string.gmatch(cli_string, "[^,]+") do
 	local left, right = string.match(assignment, "(.-)=(.*)")
 	if left and right then
 		wesnoth.set_variable(left, right)

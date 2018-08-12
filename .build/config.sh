b6548f06753baa6de3d37996c97d01440bdf120f
Vasya N <n1m5spm@yandex.com>
2018-08-12 20:11:48 +0300

    fix upload function

--- .build/config.sh
+++ .build/config.sh
@@ -7,16 +7,15 @@ description() {
 	cd ..
 }
 
-upload_to_wesnoth_versions=(1.14)
+upload_to_wesnoth_versions=("1.14")
 
 declare -A titles=(
-	[12]="Creep Wars New"
-	[14]="Creep Wars"
+	["1.14"]="Creep Wars"
 )
 
 upload() {
-	for ver in "${versions[@]}"; do
-		wesnoth_addon_manager --port "1.$ver.x" \
+	for ver in "${upload_to_wesnoth_versions[@]}"; do
+		wesnoth_addon_manager --port "$ver.x" \
 			--pbl-key "version" "$(version)" \
 			--pbl-key "description" "$(description)" \
 			--pbl-key "passphrase" "$(passphrase)" \

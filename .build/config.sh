46264da956ee1115f037155e5f626432aadf79f0
Vasya N <n1m5spm@yandex.com>
2018-09-13 15:51:59 +0300

    clean up

--- .build/config.sh
+++ .build/config.sh
@@ -8,18 +8,3 @@ description() {
 }
 
 upload_to_wesnoth_versions=("1.14")
-
-declare -A titles=(
-	["1.14"]="Creep Wars"
-)
-
-upload() {
-	for ver in "${upload_to_wesnoth_versions[@]}"; do
-		wesnoth_addon_manager --port "$ver.x" \
-			--pbl-key "version" "$(version)" \
-			--pbl-key "description" "$(description)" \
-			--pbl-key "passphrase" "$(passphrase)" \
-			--pbl-key "title" "${titles[$ver]}" \
-			--upload Creep_War_Dev
-	done
-}

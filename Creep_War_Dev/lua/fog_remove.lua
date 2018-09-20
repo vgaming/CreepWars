d4d5fc265ec348d65f47b5da9c68c01267683ff9
Vasya N <n1m5spm@yandex.com>
2018-09-20 17:45:42 +0300

    clean up

--- Creep_War_Dev/lua/fog_remove.lua
+++ Creep_War_Dev/lua/fog_remove.lua
@@ -6,16 +6,13 @@ local creepwars = creepwars
 local T = wesnoth.require("lua/helper.lua").set_wml_tag_metatable {}
 local is_ai_array = creepwars.is_ai_array
 
--- wesnoth-1.12 seems to be a bit buggy, we'll clear fog with multiturn = true AND false.
 local function lift_fog(x, y)
 	wesnoth.wml_actions.lift_fog { x = x, y = y, multiturn = true }
-	wesnoth.wml_actions.lift_fog { x = x, y = y, multiturn = false }
 end
 
 for _, unit in ipairs(wesnoth.get_units { canrecruit = true }) do
 	if is_ai_array[unit.side] == true then
 		local ability = T.name_only {
-			id = "creepwars_guard",
 			name = "guard",
 			description = "All team members lose if this unit dies. \n"
 				.. "You can heal and unpoison your guard at the Shop."

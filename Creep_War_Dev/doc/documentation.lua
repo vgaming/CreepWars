0f5b83242d16a73aefb5061a4317ca428b7fdbb5
Vasya N <n1m5spm@yandex.com>
2019-01-14 08:27:32 +0100

    more gold is given in late game
    this makes games faster (reduces playing time)

--- Creep_War_Dev/doc/documentation.lua
+++ Creep_War_Dev/doc/documentation.lua
@@ -40,7 +40,7 @@ if wesnoth then
 		text = "<span color='#FFFFFF'>Mirror style: " .. creepwars.mirror_style .. "</span>"
 	}
 
-	local recent = "Recent changes: re-designed weapon system, lvl0 leaders have ZoC."
+	local recent = "Recent changes: more gold is given in late game."
 	wesnoth.message("Creep Wars", "Press Ctrl J to see game rules. " .. recent)
 
 	local T = wesnoth.require("lua/helper.lua").set_wml_tag_metatable {}
